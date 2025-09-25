999aa6e003bd78e119df0b8ca59a5d1efca83f8d
Barigbue Nbira <barigbuenbira@gmail.com>
2025-09-25 09:10:25 +0100

    feat(errs): add structured HTTP error handling with support for actions and field errors
    Signed-off-by: Barigbue Nbira <barigbuenbira@gmail.com>

---
+++ internal/errs/http.go
@@ -0,0 +1,66 @@
+package errs
+
+import "strings"
+
+// Action and ActionType define a structured way of communicating "what the client should do next"
+// when an error or special condition occurs. Instead of returning only a plain error message,
+// the backend can send an actionable response that the frontend can interpret and execute.
+//
+// Example use case: If a user's session has expired, the backend returns an Action with
+// Type = "redirect", Message = "Your session has expired. Please log in again.",
+// and Value = "/login". The frontend, upon seeing ActionTypeRedirect, knows it should
+// navigate the user to the login page.
+type ActionType string
+
+const (
+	ActionTypeRedirect ActionType = "redirect"
+)
+
+type Action struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+	Value   string `json:"value"`
+}
+
+// for input field specific errors
+type FieldError struct {
+	Field string `json:"field"`
+	Error string `json:"error"`
+}
+
+// HttpError represents a structured error for HTTP responses.
+type HttpError struct {
+	Code     string `json:"code"`
+	Status   int    `json:"status"`
+	Message  string `json:"message"`
+	Override bool `json:"override"`
+	Action   *Action `json:"action,omitempty"`
+	Errors  []FieldError `json:"fields,omitempty"`
+}
+
+func (e *HttpError) Error() string {
+	return e.Message
+}
+
+func (e *HttpError) Is(target error) bool {
+	_, ok:= target.(*HttpError)
+
+return ok
+}
+
+func (e *HttpError) WithMessage(message string) *HttpError{
+	return &HttpError{
+		Code:  e.Code,
+		Status: e.Status,
+		Message: message,
+		Override: e.Override,
+		Action: e.Action,
+		Errors: e.Errors,
+		
+	}
+}
+
+func MakeUpperCaseWithUnderscores(s string) string {
+	return strings.ToUpper( strings.ReplaceAll(s, " ", "_") )
+}
+
---
+++ internal/errs/type.go
@@ -0,0 +1,67 @@
+package errs
+
+import "net/http"
+
+func ForbididdenError(message string, override bool) *HttpError {
+	return &HttpError{
+		Code: MakeUpperCaseWithUnderscores(http.StatusText(http.StatusForbidden)),
+		Message: message,
+		Status: http.StatusForbidden,
+		Override: override,
+	}
+}
+
+
+func UnauthorizedError(message string, override bool) *HttpError {
+	return &HttpError{
+		Code: MakeUpperCaseWithUnderscores(http.StatusText(http.StatusUnauthorized)),
+		Message: message,
+		Status: http.StatusUnauthorized,
+		Override: override,
+	}
+}
+
+func BadRequestError(message string, override bool, code *string, errors []FieldError, action *Action) *HttpError{
+	formattedCode := MakeUpperCaseWithUnderscores(http.StatusText(http.StatusBadRequest))
+
+	if code != nil {
+		formattedCode = *code
+	}
+	
+	return &HttpError{
+		Code: formattedCode,
+		Message: message,
+		Status: http.StatusBadRequest,
+		Override: override,
+		Errors: errors,
+		Action: action,
+	}
+}
+
+func InternalServerError() *HttpError{
+	return &HttpError{
+		Code: MakeUpperCaseWithUnderscores(http.StatusText(http.StatusInternalServerError)),
+		Message: http.StatusText(http.StatusInternalServerError),
+		Status: http.StatusInternalServerError,
+		Override: false,
+	}
+}
+
+func NotFoundError(message string, override bool, code *string) *HttpError{
+	formattedCode := MakeUpperCaseWithUnderscores(http.StatusText(http.StatusNotFound))
+
+	if code != nil {
+		formattedCode = *code
+	}
+
+	return &HttpError{
+		Code: formattedCode,
+		Message: message,
+		Status: http.StatusNotFound,
+		Override: override,
+	}
+}
+
+func validationError(err error) *HttpError{
+	return BadRequestError("validation unsuccessful", false, nil, nil, nil)
+}
