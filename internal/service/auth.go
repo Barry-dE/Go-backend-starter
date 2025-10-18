da5cc6ad0178ab40b0c178e81533c697846496cc
Barigbue Nbira <barigbuenbira@gmail.com>
2025-10-18 17:28:37 +0100

    feat(service): add centralized service initialization for auth and job services
    Signed-off-by: Barigbue Nbira <barigbuenbira@gmail.com>

---
+++ internal/service/auth.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"github.com/Barry-dE/go-backend-boilerplate/internal/server"
+	"github.com/clerk/clerk-sdk-go/v2"
+)
+
+type AuthService struct {
+	server *server.Server
+}
+
+func NewAuthService(s *server.Server) *AuthService{
+	clerk.SetKey(s.Config.Auth.SecretKey)
+	return &AuthService{
+		server: s,
+	}
+}
---
+++ internal/service/services.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"github.com/Barry-dE/go-backend-boilerplate/internal/lib/job"
+	"github.com/Barry-dE/go-backend-boilerplate/internal/repository"
+	"github.com/Barry-dE/go-backend-boilerplate/internal/server"
+)
+
+type Services struct {
+	AuthService *AuthService
+	Job         *job.JobService
+}
+
+func NewService(s *server.Server, repos *repository.Repositories) (*Services, error){
+	authService := NewAuthService(s)
+
+	return &Services{
+		AuthService: authService,
+		Job: s.Job,
+	}, nil
+}
